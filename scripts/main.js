ac0e0275ce9725d7b21be1d46767a741be29f904
Craig Orcutt <craigorcutt@Craigs-MacBook-Pro.local>
2018-01-04 15:28:42 -0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const factory = require('./factory');
+const format = require('./formatter');
+// const acmeData = [];
+
+// factory.getCategories()
+// .then( (categories) => {
+//     console.log(categories);
+//     // added cats to empty array
+//     acmeData.push(categories);
+//     return factory.getTypes();
+// })
+// .then( (types) =>{
+//     console.log(types);
+//     // added types to empty array
+//     acmeData.push(types);
+//     return factory.getProducts();
+// })
+// .then( (products) =>{
+//     // added products to empty array
+//     acmeData.push(products);
+//     console.log(products);
+//     // logged array that has cats, types, and prods in an array. an array of all the arrays
+//     console.log(acmeData);
+// })
+// .catch( (err) => {
+//     console.log(`there was a problem: ${err.status} ${err.statusText}`);
+// });
+
+let promArr = [
+    factory.getCategories(),
+    factory.getTypes(), 
+    factory.getProducts()
+];
+
+Promise.all(promArr)
+.then( (dataArr) =>{
+    format.formatData(dataArr);
+})
+.catch((err) =>{
+    console.log(err);
+});
+
+
+// UI interface w/dropdown
+
+// Grab the selected value
+
+// Make XHR Request(s) for data
+
+
+
+// Display data to the user
+
+
+
